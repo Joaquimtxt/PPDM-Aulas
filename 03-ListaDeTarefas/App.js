faa0bd4070486686fcb9f855093efffcd4582511
Joaquim <jocaq124@gmail.com>
2025-08-27 10:34:07 -0300

    Melhorado a estilização, estilizando vários elementos da página

--- 03-ListaDeTarefas/App.js
+++ 03-ListaDeTarefas/App.js
@@ -1,10 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  View,
+} from "react-native";
 
 export default function App() {
+  // {toggleIcon, setToggleIcon}useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Cabeçalho */}
+      <View style={styles.topBar}>
+        <Text style={styles.topBarTitle}>Minhas Tarefas</Text>
+        <TouchableOpacity>
+          <Text>🌛</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.card}>
+        {/* Local onde o user insere novas tarefas */}
+        <TextInput
+          style={styles.input}
+          placeholder="Adicionar nova tarefa..."
+        />
+        <TouchableOpacity style={styles.addButton}>
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+      </View>
+      {/* Lista de tarefas do User */}
+      <FlatList
+        style={styles.flatList}
+        ListEmptyComponent={() => (
+          <Text style={styles.emptyListText}>
+            Nenhuma tarefa adicionada ainda.
+          </Text>
+        )}
+        contentContainerStyle={styles.flatListContent}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +47,105 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#e0f7fa",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  topBar: {
+    backgroundColor:"#FFF",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingTop: 50, //Ajuste para a barra de status
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "rgba(0,0,0,0.1)",
+  },
+  topBarTitle: {
+    color: "#00796b",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  card: {
+    backgroundColor:"#FFF",
+    color:"#000",
+    shadowColor: "#000",
+    margin: 20,
+    borderRadius: 15,
+    padding: 20,
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 10,
+  },
+  input: {
+    backgroundColor:"#FCFCFC",
+    color:"#333",
+    borderColor:"#b0bec5",
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 20,
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  addButton: {
+    backgroundColor:"#009688",
+    paddingVertical: 12,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  flatListContent: {
+    paddingBottom: 10, //Espaçamento no final da Lista
+  },
+  taskItem: {
+    backgroundColor:"FFF",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 10,
+    padding: 15,
+    marginVertical: 10,
+    marginHorizontal: 15,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 5,
+    backgroundColor: "#FFF",
+    color: "#333",
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.1",
+  },
+  taskTextContainer: {
+    flex: 1, //Permite que o texto ocupe o espaço disponível
+    marginRight: 10,
+  },
+  taskText: {
+    color:"#333",
+    fontSize: 18,
+    flexWrap: "wrap", //Permite que o texto quebre ao chegar no final da tela
+  },
+  completedTaskItem: {
+    textDecorationLine: "line-through", //risca o texto
+    opacity: 0.6,
+  },
+  deleteButton: {
+    padding: 8,
+    borderRadius: 5,
+  },
+  deleteButtonText: {
+    // color:"#fff",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  emptyListText: {
+    color:"#9e9e9e",
+    textAlign: "center",
+    marginTop: 50,
+    fontSize: 16,
   },
 });
